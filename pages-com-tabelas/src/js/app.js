bb14ee5f2413e8c3c2beb34dbd0ea63157a242e0
Your Name <you@example.com>
2020-04-28 22:02:53 -0300

    Projeto curso web

---
+++ pages-com-tabelas/src/js/app.js
@@ -0,0 +1,16 @@
+let nome = "felipe ";
+let idade = 29;
+let peso = 110;
+let humano = true;
+
+let aluno = ["Benjamin", "Felipe", "Jessica"];
+
+let alunos = {
+    nome: "Felipe",
+    idade:29,
+    peso:110,
+    humano:true
+
+};
+
+console.log(alunos.nome, aluno[1]);
